d6932360f538f62981b4701063f0683defd13681
ayush <ayushtiwariiitr@gmail.com>
2015-02-17 18:45:41 +0530

    Changes in manage.py to integrate settings

--- sarvahar/manage.py
+++ sarvahar/manage.py
@@ -4,11 +4,9 @@ import sys
 
 import dotenv
 dotenv.read_dotenv()
-sys.path.append('sarvahar.settings.settings_harshit')
 
-#import pdb;pdb.set_trace()
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sarvahar.sarvahar.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sarvahar.settings")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
